6e08cb7b1c81384b42ef2769aa7058d48df4235c
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2021-01-28 20:47:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+http.listen(3000, () => {
+    console.log('App Started on Port 3000 ✅')
+})
