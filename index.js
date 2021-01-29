8e04a775e63358d75a001e0239d988655635c359
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2021-01-28 21:42:42 -0300

    connect to server

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.set('view engine', 'ejs');
+io.on("connection",(socket) => {
+    socket.on("boasvindas", (data) => {
+        console.log("EXECUTANDO EVENTO DE BOAS VINDAS!");
+        console.log(data);
+    })
+
+    socket.on("palavra", (data) => {
+        console.log(data);
+        socket.emit("resultado", data + "Guia do Programador");
+    })
+});
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("index");
 })
 
 http.listen(3000, () => {
-    console.log('App Started on Port 3000 ✅')
+    console.log("App Started on Port 3000 ✅")
 })
